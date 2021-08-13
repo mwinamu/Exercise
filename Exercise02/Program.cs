7e721ef00a39c4db3a55d5b27c549e74179a7d99
Higgins Mwinamu Mugaisi <HMUGAISI@safaricom.co.ke>
2021-08-13 17:48:44 +0300

    first push

---
+++ Exercise02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+using Exercise01;
+
+namespace Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number to convert to words: ");
+            string numb= Console.ReadLine().Replace(",","").Trim();
+            Console.WriteLine(numb);
+            double n = double.Parse(numb);
+            Console.WriteLine("{0}", n.Towards());
+        }
+    }
+}
